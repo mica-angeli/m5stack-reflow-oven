4a057cd801f83e6029a3847640f79bb56f2e51fc
Ricardo Angeli <rangeli93@gmail.com>
2019-02-10 02:33:53 -0500

    Added the other two temperature reading functions to MCP9600 library.

--- components/mcp9600/mcp9600.c
+++ components/mcp9600/mcp9600.c
@@ -4,6 +4,18 @@
 
 #include "mcp9600.h"
 
+static float to_temperature(uint16_t raw_value) {
+  float value;
+  if(raw_value & 0x8000) {
+    value = (raw_value >> 8) * 16.0f + (raw_value & 0x00ff)/16.0f - 4096.0f;
+  }
+  else
+  {
+    value = (raw_value >> 8) * 16.0f + (raw_value & 0x00ff)/16.0f;
+  }
+  return value;
+}
+
 static esp_err_t Mcp_write_byte(const Mcp9600 * this, uint8_t reg, uint8_t data) {
   i2c_cmd_handle_t cmd = i2c_cmd_link_create();
   esp_err_t ret;
@@ -149,13 +161,31 @@ esp_err_t Mcp_read_hot_junc(const Mcp9600 *this, float *temperature) {
     return ESP_FAIL;
   }
 
-  if(read_value & 0x8000) {
-    *temperature = (read_value>>8) * 16.0f + (read_value & 0x00ff)/16.0f - 4096.0f;
+  *temperature = to_temperature(read_value);
+
+  return ESP_OK;
+}
+
+esp_err_t Mcp_read_junc_temp_delta(const Mcp9600 *this, float *temperature) {
+  *temperature = 0.0f;
+  uint16_t read_value = 0;
+  if(ESP_OK != Mcp_read_16bit(this, JUNCTION_TEMP_DELTA_REG_ADDR, &read_value)) {
+    return ESP_FAIL;
   }
-  else
-  {
-    *temperature = (read_value>>8) * 16.0f + (read_value & 0x00ff)/16.0f;
+
+  *temperature = to_temperature(read_value);
+
+  return ESP_OK;
+}
+
+esp_err_t Mcp_read_cold_junc(const Mcp9600 *this, float *temperature) {
+  *temperature = 0.0f;
+  uint16_t read_value = 0;
+  if(ESP_OK != Mcp_read_16bit(this, COLD_JUNCTION_TEMP_REG_ADDR, &read_value)) {
+    return ESP_FAIL;
   }
+
+  *temperature = to_temperature(read_value);
 
   return ESP_OK;
 }
